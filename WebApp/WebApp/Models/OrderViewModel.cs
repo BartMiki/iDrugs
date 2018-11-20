30180dbc284b2c166d5c29f8a4bd5a81153f2261
Bartosz Mikulski <bartmiki97@gmail.com>
2018-11-20 21:33:36 +0100

    All changes

---
+++ WebApp/WebApp/Models/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+        public int ApothecaryId { get; set; }
+        public DateTime OrderDate { get; set; }
+        //public IEnumerable<> OrderItems { get; set; }
+    }
+}
